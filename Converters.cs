49de4086c8a222bded6d1d603c908d0b41ef3813
Aaron Vazquez <Aaron.Vazquez@live.com>
2019-01-10 13:17:23 -0500

    First Commit

---
+++ Converters.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+using System.Xml.Serialization;
+
+namespace AL.Tools
+{
+    public static class Converters
+    {
+        readonly static string[] SizeSuffixes = { "bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB" };
+
+        public static string ToXML<T>(this T obj)
+        {
+            using (var stringWriter = new StringWriter(new StringBuilder()))
+            {
+                var xmlSerializer = new XmlSerializer(typeof(T));
+                xmlSerializer.Serialize(stringWriter, obj);
+                var XmlString = stringWriter.ToString();
+                XmlString = XmlString.Replace("<?xml version=\"1.0\" encoding=\"utf - 16\"?>", "");
+                XmlString = XmlString.Replace("<?xml version=\"1.0\" encoding=\"utf-16\"?>", "");
+                XmlString = XmlString.Replace(" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\"", "");
+                return XmlString;
+            }
+        }
+
+        public static T FromXML<T>(string xml)
+        {
+            using (StringReader stringReader = new StringReader(xml))
+            {
+                XmlSerializer serializer = new XmlSerializer(typeof(T));
+                return (T) serializer.Deserialize(stringReader);
+            }
+        }
+
+        public static string SerializeObject<T>(this T toSerialize)
+        {
+            var xmlSerializer = new XmlSerializer(toSerialize.GetType());
+            var textWriter = new Utf8StringWriter();
+
+            xmlSerializer.Serialize(textWriter, toSerialize);
+            return textWriter.ToString();
+        }
+
+        public static Boolean ToBoolean(this String str)
+        {
+            switch (str.ToLower())
+            {
+                case "true":
+                    return true;
+                case "t":
+                    return true;
+                case "1":
+                    return true;
+                case "0":
+                    return false;
+                case "false":
+                    return false;
+                case "f":
+                    return false;
+                default:
+                    throw new InvalidCastException("You can't cast a weird value to a bool!");
+            }
+        }
+
+        public static Boolean ToBoolean(this Object obj)
+        {
+            return Convert.ToBoolean(obj);
+        }
+
+        public static DateTime ToDateTime(this Object obj)
+        {
+            return Convert.ToDateTime(obj);
+        }
+
+        public static int ToInt(this Object obj)
+        {
+            return Convert.ToInt32(obj);
+        }
+
+        public static float ToSingle(this Double number)
+        {
+            return Convert.ToSingle(number);
+        }
+
+        public static Image ToImage(this byte[] img)
+        {
+            byte[] imageData = (byte[]) img;
+
+            Image newImage;
+
+            using (var ms = new MemoryStream(imageData, 0, imageData.Length))
+            {
+                ms.Write(imageData, 0, imageData.Length);
+
+                newImage = Image.FromStream(ms, true);
+            }
+
+            return newImage;
+        }
+
+        public static Guid ToGuid(this string str)
+        {
+            return new Guid(str);
+        }
+
+        public static DataTable ToDataTable<T>(this IList<T> data)
+        {
+            PropertyDescriptorCollection props = TypeDescriptor.GetProperties(typeof(T));
+            DataTable table = new DataTable();
+            for (int i = 0; i < props.Count; i++)
+            {
+                PropertyDescriptor prop = props[i];
+                table.Columns.Add(prop.Name, prop.PropertyType);
+            }
+            object[] values = new object[props.Count];
+            foreach (T item in data)
+            {
+                for (int i = 0; i < values.Length; i++)
+                {
+                    values[i] = props[i].GetValue(item);
+                }
+                table.Rows.Add(values);
+            }
+            return table;
+        }
+
+        public static String ToReadableFileSize(this Int64 Size)
+        {
+            const long Base = 1024;
+            //In case negative value is provided
+            Size = Math.Abs(Size);
+            //Check for zero
+            if (Size == 0) { return "0.0 bytes"; }
+
+            //Get the unit based on the logarithm of the number
+            int Magnitude = (int) Math.Log(Size, Base);
+
+            //Get the size in the unit
+            double adjustedSize = (Size / Math.Pow(Base, Magnitude));
+
+            //Return readable string
+            return string.Format("{0:n2} {1}", adjustedSize, SizeSuffixes[Magnitude]);
+        }
+
+        public static string ToJSON(this object obj)
+        {
+            var srlz = new JavaScriptSerializer();
+            return srlz.Serialize(obj);
+        }
+
+        public static string ToJSON(this object obj, int recursionDepth)
+        {
+            var srlz = new JavaScriptSerializer();
+            srlz.RecursionLimit = recursionDepth;
+            return srlz.Serialize(obj);
+        }
+
+        public static T JSONtoList<T>(this object jsonObj)
+        {
+            var _jsserializer = new JavaScriptSerializer();
+            return _jsserializer.Deserialize<T>(jsonObj as string);
+        }
+
+        public static IPAddress ToIPAddress(this String str)
+        {
+            return IPAddress.Parse(str);
+        }
+    }
+
+    public class Utf8StringWriter : StringWriter
+    {
+        // Use UTF8 encoding but write no BOM to the wire
+        public override Encoding Encoding
+        {
+            get { return new UTF8Encoding(false); } // in real code I'll cache this encoding.
+        }
+    }
+}
---
+++ Extensions.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AL.Tools
+{
+    public static class Extensions
+    {
+        /// <summary>
+        /// Extension to validate whether a string is empty or not, checks for:
+        /// <list type="bullet">
+        /// <item><code>NULL</code></item>
+        /// <item><code>String.Empty</code></item>
+        /// <item>White Space</item>
+        /// </list>
+        /// </summary>
+        /// <returns>Boolean</returns>
+        public static bool IsEmpty(this string str)
+        {
+            return (String.IsNullOrEmpty(str) && String.IsNullOrWhiteSpace(str));
+        }
+
+        /// <summary>
+        /// Extension to validate whether a string is empty or not, checks for:
+        /// <list type="bullet">
+        /// <item><code>NULL</code></item>
+        /// <item><code>String.Empty</code></item>
+        /// <item>White Space</item>
+        /// </list>
+        /// Also if specified, performs a <code>Trim</code> on the String prior validation.
+        /// </summary>
+        /// <param name="Trim">Specify as <code>True</code> to perform a <code>Trim</code>.</param>
+        /// <returns>Boolean</returns>
+        public static bool IsEmpty(this string str, bool Trim)
+        {
+            if (Trim)
+                str = str.Trim();
+            return (String.IsNullOrEmpty(str) && String.IsNullOrWhiteSpace(str));
+        }
+
+        /// <summary>
+        /// Extension to validate wether a string has information.
+        /// </summary>
+        /// <returns>Boolean</returns>
+        public static bool IsNotEmpty(this string str)
+        {
+            return !IsEmpty(str);
+        }
+
+        /// <summary>
+        /// Extension to validate if an Object is <code>NULL</code>
+        /// </summary>
+        /// <returns>Boolean</returns>
+        public static bool IsNull(this object obj)
+        {
+            return (obj == null);
+        }
+
+        /// <summary>
+        /// Extension to validate if an Object is not <code>NULL</code> 
+        /// </summary>
+        /// <returns>Boolean</returns>
+        public static bool IsNotNull(this object obj)
+        {
+            return !IsNull(obj);
+        }
+
+        /// <summary>
+        /// Extension to validate wether the content of a string is a number.
+        /// </summary>
+        /// <returns>Boolean</returns>
+        public static bool IsNumber(this String str)
+        {
+            try
+            {
+                Convert.ToInt64(str);
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// Extension to validate wether the content of a string is not a number.
+        /// </summary>
+        /// <returns>Boolean</returns>
+        public static bool IsNotANumber(this String str)
+        {
+            return !IsNumber(str);
+        }
+
+        /// <summary>
+        /// Extension to validate wether the content of a string is a DateTime.
+        /// </summary>
+        /// <returns>Boolean</returns>
+        public static bool IsDateTime(this String str)
+        {
+            try
+            {
+                Convert.ToDateTime(str);
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// Extension to validate wether the content of a string is not a DateTime.
+        /// </summary>
+        /// <returns>Boolean</returns>
+        public static bool IsNotDateTime(this String str)
+        {
+            return !IsDateTime(str);
+        }
+
+        /// <summary>
+        /// Extension to validate wether a DateTime has the default value "MinDate"
+        /// </summary>
+        /// <returns>Boolean</returns>
+        public static bool IsMinDate(this DateTime dt)
+        {
+            return (dt == DateTime.MinValue);
+        }
+
+        /// <summary>
+        /// Extension to validate wether a DateTime has a value different than "MinDate"
+        /// </summary>
+        /// <returns>Boolean</returns>
+        public static bool IsNotMinDate(this DateTime dt)
+        {
+            return !IsMinDate(dt);
+        }
+
+        /// <summary>
+        /// Extension to validate if a DataSet has DataTables
+        /// </summary>
+        /// <returns>Boolean</returns>
+        public static bool HasTables(this DataSet dsData)
+        {
+            return (dsData != null && dsData.Tables.Count > 0);
+        }
+
+        /// <summary>
+        /// Extension that returns wether the DataSet has at least 1 Table and that that the first DataTable has records.
+        /// </summary>
+        /// <returns>Boolean</returns>
+        public static bool HasRows(this DataSet dsData)
+        {
+            return (dsData != null && dsData.Tables.Count > 0 && dsData.Tables[0].Rows.Count > 0);
+        }
+
+        /// <summary>
+        /// Extension that returns wether the DataTable has records.
+        /// </summary>
+        /// <returns>Boolean</returns>
+        public static bool HasRows(this DataTable dtData)
+        {
+            return (dtData.Rows.Count > 0);
+        }
+
+        /// <summary>
+        /// Extension to validate wether the content of a string is an IP Address.
+        /// </summary>
+        /// <returns>Boolean</returns>
+        public static bool IsIPAddress(this String str)
+        {
+            try
+            {
+                IPAddress.Parse(str);
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// Extension to validate wether the content of a string is not an IP Address.
+        /// </summary>
+        /// <returns>Boolean</returns>
+        public static bool IsNotIPAddress(this String str)
+        {
+            return !IsIPAddress(str);
+        }
+    }
+}
